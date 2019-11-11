47a9420e46d20c392c5206cc5d4f13b8025c8ec5
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-11-11 21:38:36 +0100

    python firestore integration

---
+++ app.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBFxCC7ALGXsA4WAaKbvx9MDxOmWVFDiC0",
+    authDomain: "chargen-13eae.firebaseapp.com",
+    databaseURL: "https://chargen-13eae.firebaseio.com",
+    projectId: "chargen-13eae",
+    storageBucket: "chargen-13eae.appspot.com",
+    messagingSenderId: "453800657527",
+    appId: "1:453800657527:web:48acbb4ffe8568f0f0bc72"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const btnList = document.querySelectorAll('.btn')
+
+btnList.forEach((btn) => {
+    btn.addEventListener('click', (evt) => {
+
+    })
+})
